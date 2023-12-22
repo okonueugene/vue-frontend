9045ebce8488ee6cf6a69d01551ecf463a37b75e
okonueugene <tylarrex67@gmail.com>
2023-12-22 10:52:26 +0300

    remove console

--- src/components/views/auth/LoginPage.vue
+++ src/components/views/auth/LoginPage.vue
@@ -2,8 +2,8 @@
   <section class="vh-100">
     <div class="container-fluid h-custom">
       <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="header text-center fs-2">Library</div>
         <div class="col-md-9 col-lg-6 col-xl-5">
-          <span>{{ api }}</span>
           <img
             src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
             class="img-fluid"
